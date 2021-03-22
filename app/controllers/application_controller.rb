d27031f38fac57d3550b3a3dc8a6f31e3c174249
ueki397 <uekijunh522@gmail.com>
2021-03-22 14:18:18 +0900

    backup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  # セキュリティ関連の記述
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
